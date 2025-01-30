d9e88c6eaffae2d739567cf6e1f3776c6ebe5e70
Noia <edwin.lecomte31@gmail.com>
2025-01-30 12:03:56 +0100

    [ADD] #9 adding logger dll and form project skeleton

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasySave
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //LoggerUtility.WriteLog(LoggerUtility.INFO, "EasySave Application Start.");
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
