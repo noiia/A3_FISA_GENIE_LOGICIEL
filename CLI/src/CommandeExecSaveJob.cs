c3095bdb629277c181794775e34fc7a9ac23f86f
RoutouX <thomasschmittadresse@outlook.com>
2025-02-06 11:15:54 +0100

    [ FEATURE ] Add in Commande ExecSaveJob the mult arg likes ; or ,

--- CLI/src/CommandeExecSaveJob.cs
+++ CLI/src/CommandeExecSaveJob.cs
@@ -11,15 +11,71 @@ namespace CLI
 
         public override void Action(string[] args)
         {
-            int r = ServiceExecSaveJob.Run(args);
-            switch (r)
+            int intType;
+            int r;
+            if(args[0].Contains(";"))
             {
-                case ServiceAddSaveJob.OK:
-                    Console.WriteLine(ConsoleColors.Green + "\t Save Job has benn created" + ConsoleColors.Reset);
-                    return;
-                case ServiceAddSaveJob.BAD_ARGS:
-                    Console.WriteLine(ConsoleColors.Red + "\t Bad arguments, require Add-Savejob <name> <source> <destination>" + ConsoleColors.Reset);
-                    return;
+                string[] listIds = args[0].Split(';');
+                foreach (var id in listIds)
+                {
+                    string[] arg = [id];
+                    r = ServiceExecSaveJob.Run(arg);
+                    switch (r)
+                    {
+                        case ServiceExecSaveJob.OK:
+                            Console.WriteLine(ConsoleColors.Green + "\t Save Job has benn created" + ConsoleColors.Reset);
+                            return;
+                        case ServiceExecSaveJob.BAD_ARGS:
+                            Console.WriteLine(ConsoleColors.Red + "\t Bad arguments, require Exec-Savejob <id> | <id>;<id>;<id> | <id>,<id>" + ConsoleColors.Reset);
+                            return;
+                        case ServiceExecSaveJob.JOB_DOES_NOT_EXIST:
+                            Console.WriteLine(ConsoleColors.Red + "\t This job does not exist" + ConsoleColors.Reset);
+                            return;
+                    }
+                }
+            }
+            else if(args[0].Contains(","))
+            {
+                int min = int.Parse(args[1].Split(',')[0]);
+                int max = int.Parse(args[1].Split(',')[1]);
+                for (int i = min; i <= max; i++)
+                {
+                    string[] arg = [i.ToString()];
+                    r = ServiceExecSaveJob.Run(arg);
+                    switch (r)
+                    {
+                        case ServiceExecSaveJob.OK:
+                            Console.WriteLine(ConsoleColors.Green + "\t Save Job has benn created" + ConsoleColors.Reset);
+                            return;
+                        case ServiceExecSaveJob.BAD_ARGS:
+                            Console.WriteLine(ConsoleColors.Red + "\t Bad arguments, require Exec-Savejob <id> | <id>;<id>;<id> | <id>,<id>" + ConsoleColors.Reset);
+                            return;
+                        case ServiceExecSaveJob.JOB_DOES_NOT_EXIST:
+                            Console.WriteLine(ConsoleColors.Red + "\t This job does not exist" + ConsoleColors.Reset);
+                            return;
+                    } 
+                }
+
+            }
+            else if(int.TryParse(args[0], out intType))
+            {
+                r = ServiceExecSaveJob.Run(args);
+                switch (r)
+                {
+                    case ServiceExecSaveJob.OK:
+                        Console.WriteLine(ConsoleColors.Green + "\t Save Job has benn created" + ConsoleColors.Reset);
+                        return;
+                    case ServiceExecSaveJob.BAD_ARGS:
+                        Console.WriteLine(ConsoleColors.Red + "\t Bad arguments, require Exec-Savejob <id> | <id>;<id>;<id> | <id>,<id>" + ConsoleColors.Reset);
+                        return;
+                    case ServiceExecSaveJob.JOB_DOES_NOT_EXIST:
+                        Console.WriteLine(ConsoleColors.Red + "\t This job does not exist" + ConsoleColors.Reset);
+                        return;
+                }   
+            }
+            else
+            {
+                Console.WriteLine(ConsoleColors.Red + "\t Bad arguments, require Exec-Savejob <id> | <id>;<id>;<id> | <id>,<id>" + ConsoleColors.Reset);
             }
         }
 
