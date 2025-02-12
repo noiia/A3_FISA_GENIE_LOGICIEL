76d362c9d19f853bf907e6dbaeb200d1d69fe63f
Noia <edwin.lecomte31@gmail.com>
2025-02-12 15:13:29 +0100

    [IMP] adding return to services

--- DeleteSaveJob/Program.cs
+++ DeleteSaveJob/Program.cs
@@ -5,12 +5,12 @@ namespace DeleteSaveJob
     public class Program
     {
         // DeleteSaveJob <id_savejob> <args>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             Configuration config = new Configuration(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\EasySave\\" + "config.json");
             // config.GetSaveJob(args[0]);
             //#TODO faire en sorte que ça ne fonctionne pas qu'avec les id mais aussi les noms
-             return ServiceDeleteSaveJob.Run(args, config);
+            return ServiceDeleteSaveJob.Run(args, config);
         }
     }
 }
