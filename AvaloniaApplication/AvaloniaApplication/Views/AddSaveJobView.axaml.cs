7acb6df679e1adaa0c28df50e30b253f44272370
Noia <edwin.lecomte31@gmail.com>
2025-02-14 10:56:13 +0100

    [IMP] adding table on home page and dedicated views addsavejob and settings

---
+++ AvaloniaApplication/AvaloniaApplication/Views/AddSaveJobView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication.Views;
+
+public partial class AddSaveJobView : UserControl
+{
+    public AddSaveJobView()
+    {
+        InitializeComponent();
+    }
+}
