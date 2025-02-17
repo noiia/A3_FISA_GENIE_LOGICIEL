c57ab5694f13c803907fff2cabf5b6bd3927f156
Noia <edwin.lecomte31@gmail.com>
2025-02-18 00:04:51 +0100

    [IMP] changing Job traductions to new version

--- Job/Controller/ExecuteSaveJob.cs
+++ Job/Controller/ExecuteSaveJob.cs
@@ -8,48 +8,24 @@ namespace Controller;
 
 public class ExecuteSaveJob
 {
-    public static string Execute(Configuration configuration, string[] args)
+    public static string Execute(string[] args)
     {
-        Func<List<string>> languageFunc = Translation.SelectLanguage(configuration.GetLanguage());
-        List<string> language = languageFunc();
-        
-        LoggerUtility.WriteLog(LoggerUtility.Info, $"{language[13]} {string.Join(" ", args)}");
+        LoggerUtility.WriteLog(LoggerUtility.Info, $"{Translation.Translator.GetString("SjExecWith")} {string.Join(" ", args)}");
         int intType;
         if(args[0].Contains(";"))
         {
             string[] listIds = args[0].Split(';');
             foreach (var id in listIds)
             {
-                ProcessStartInfo serviceAddSaveJob = new ProcessStartInfo
-                {
-                    FileName = "ExecSaveJob.exe", 
-                    Arguments = id,           
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true, 
-                    CreateNoWindow = true         
-                };
-        
-                Process processServiceAddSaveJob = new Process { StartInfo = serviceAddSaveJob };
-                processServiceAddSaveJob.Start();
-                string output = processServiceAddSaveJob.StandardOutput.ReadToEnd();
-                string error = processServiceAddSaveJob.StandardError.ReadToEnd();
-                processServiceAddSaveJob.WaitForExit();
                 
-                String.Join("Output:", output);
-                if (!string.IsNullOrWhiteSpace(error))
-                {
-                    String.Join("Error:", error);
-                }
-                
-                switch (processServiceAddSaveJob.ExitCode)
+                switch ()
                 {
                     case 1:
-                        return language[14];
+                        return Translation.Translator.GetString("SjExecSuccesfully");
                     case 2:
-                        return language[15];
+                        return Translation.Translator.GetString("ExecSjBadArgs");
                     case 3:
-                        return language[16];
+                        return $"{Translation.Translator.GetString("SjNotExist")} {id} )";
                     default:
                         return String.Empty;
                 }
@@ -61,113 +37,52 @@ public class ExecuteSaveJob
             int max = int.Parse(args[1].Split(',')[1]);
             for (int i = min; i <= max; i++)
             {
-                ProcessStartInfo serviceAddSaveJob = new ProcessStartInfo
-                {
-                    FileName = "ExecSaveJob.exe", 
-                    Arguments = i.ToString(),     
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true, 
-                    CreateNoWindow = true         
-                };
-        
-                Process processServiceAddSaveJob = new Process { StartInfo = serviceAddSaveJob };
-                processServiceAddSaveJob.Start();
-                string output = processServiceAddSaveJob.StandardOutput.ReadToEnd();
-                string error = processServiceAddSaveJob.StandardError.ReadToEnd();
-                processServiceAddSaveJob.WaitForExit();
-                
-                String.Join("Output:", output);
-                if (!string.IsNullOrWhiteSpace(error))
-                {
-                    String.Join("Error:", error);
-                }
-                
-                switch (processServiceAddSaveJob.ExitCode)
+                switch ()
                 {
                     case 1:
-                        return language[14];
+                        return Translation.Translator.GetString("SjExecSuccesfully");
                     case 2:
-                        return language[15];
+                        return Translation.Translator.GetString("ExecSjBadArgs");
                     case 3:
-                        return language[16];
+                        return $"{Translation.Translator.GetString("SjNotExist")} {i} )";
                     default:
                         return String.Empty;
                 }
             }
 
         }
-        else if(int.TryParse(args[0], out intType))
-        {
-            ProcessStartInfo serviceAddSaveJob = new ProcessStartInfo
-            {
-                FileName = "ExecSaveJob.exe", // Programme à exécuter
-                Arguments = string.Join(' ',args),           // Arguments optionnels
-                UseShellExecute = false,    // Utiliser le shell Windows (obligatoire pour certaines applications)
-                RedirectStandardOutput = true, // Capture la sortie standard
-                RedirectStandardError = true,  // Capture les erreurs
-                CreateNoWindow = true         // Évite d'afficher une fenêtre
-            };
-        
-            Process processServiceAddSaveJob = new Process { StartInfo = serviceAddSaveJob };
-            processServiceAddSaveJob.Start();
-            processServiceAddSaveJob.WaitForExit();
-            string output = processServiceAddSaveJob.StandardOutput.ReadToEnd();
-            string error = processServiceAddSaveJob.StandardError.ReadToEnd();
-            
-            String.Join("Output:", output);
-            if (!string.IsNullOrWhiteSpace(error))
-            {
-                String.Join("Error:", error);
-            }
-            
-            switch (processServiceAddSaveJob.ExitCode)
-            {
-                case 1:
-                    return language[14];
-                case 2:
-                    return language[15];
-                case 3:
-                    return language[16];
-                default:
-                    return String.Empty;
-            }   
-        }
         else
         {
-            ProcessStartInfo serviceExecSaveJob = new ProcessStartInfo
+            if(int.TryParse(args[0], out intType))
             {
-                FileName = "ExecSaveJob.exe",
-                Arguments = string.Join(' ', args),
-                UseShellExecute = false,  // Changed from true to false
-                RedirectStandardOutput = true, // Added to capture output
-                RedirectStandardError = true,  // Added to capture errors
-                CreateNoWindow = true
-            };
-            Process processServiceExecSaveJob = new Process { StartInfo = serviceExecSaveJob };
-            processServiceExecSaveJob.Start();
-            processServiceExecSaveJob.WaitForExit();
-            string output = processServiceExecSaveJob.StandardOutput.ReadToEnd();
-            string error = processServiceExecSaveJob.StandardError.ReadToEnd();
-            
-            String.Join("Output:", output);
-            if (!string.IsNullOrWhiteSpace(error))
-            {
-                String.Join("Error:", error);
+                switch ()
+                {
+                    case 1:
+                        return Translation.Translator.GetString("SjExecSuccesfully");
+                    case 2:
+                        return Translation.Translator.GetString("ExecSjBadArgs");
+                    case 3:
+                        return $"{Translation.Translator.GetString("SjNotExist")} {string.Join(' ', args)} )";
+                    default:
+                        return String.Empty;
+                }   
             }
-            
-            switch (processServiceExecSaveJob.ExitCode)
+            else
             {
-                case 1:
-                    return language[14];
-                case 2:
-                    return language[15];
-                case 3:
-                    return language[16];
-                default:
-                    return String.Empty;
-            }   
+                switch ()
+                {
+                    case 1:
+                        return Translation.Translator.GetString("SjExecSuccesfully");
+                    case 2:
+                        return Translation.Translator.GetString("ExecSjBadArgs");
+                    case 3:
+                        return $"{Translation.Translator.GetString("SjNotExist")} {string.Join(' ', args)} )";
+                    default:
+                        return String.Empty;
+                }   
+            }
         }
+
         return String.Empty;
     }
 }
